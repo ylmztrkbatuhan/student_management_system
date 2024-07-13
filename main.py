39b0a9899905a7b66f78aa758cc311d70b6a0537
oboy21 <oboy11b@gmail.com>
2024-07-13 23:17:04 +0300

    add menu bar

--- main.py
+++ main.py
@@ -1,46 +1,28 @@
-from PyQt6.QtWidgets import QApplication,QLabel, QWidget, QGridLayout,\
- QLineEdit, QPushButton
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout,\
+    QLineEdit, QPushButton, QMainWindow
+
+from PyQt6.QtGui import QAction
 
 import sys
-from datetime import datetime
-class AgeCalculator(QWidget):
+
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Age Calculator")
-        grid = QGridLayout()
+        self.setWindowTitle("Student Management System")
 
 
-        #Create widgets
-        name_label = QLabel("Name:")
-        self.name_line_edit = QLineEdit()
+        file_menu_item = self.menuBar().addMenu("&File")
+        help_menu_item = self.menuBar().addMenu("&Help")
 
-        date_birth_label = QLabel("Date of birth MM/DD/YYYY:")
-        self.date_birth_line_edit = QLineEdit()
-
-        calculate_button = QPushButton("Calculate Age")
-        calculate_button.clicked.connect(self.calculate_age)
-        self.output_label = QLabel("")
-
-        # Add widgets to grid
-        grid.addWidget(name_label, 0, 0)
-        grid.addWidget(self.name_line_edit, 0, 1)
-        grid.addWidget(date_birth_label, 1, 0)
-        grid.addWidget(self.date_birth_line_edit, 1, 1)
-        grid.addWidget(calculate_button, 2, 0, 1, 2)
-        grid.addWidget(self.output_label, 3, 0, 1, 2)
+        add_student_action = QAction("Add Student", self)
+        file_menu_item.addAction(add_student_action)
 
 
-        self.setLayout(grid)
-
-    def calculate_age(self):
-        current_year = datetime.now().year
-        date_of_birth = self.date_birth_line_edit.text()
-        year_of_birth = datetime.strptime(date_of_birth, "%m/%d/%Y").date().year
-        age = current_year - year_of_birth
-        self.output_label.setText(f"{self.name_line_edit.text()} is {age} years old")
-
+        about_action = QAction("About", self)
+        help_menu_item.addAction(about_action)
+        about_action.setMenuRole(QAction.MenuRole.NoRole)
 
 app = QApplication(sys.argv)
-age_calculator = AgeCalculator()
+age_calculator = MainWindow()
 age_calculator.show()
 sys.exit(app.exec())
