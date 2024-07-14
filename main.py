32c08fa76d8d5e2d217ae73dd62562af7d27365b
oboy21 <oboy11b@gmail.com>
2024-07-14 07:38:05 +0300

    creating table structure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout,\
-    QLineEdit, QPushButton, QMainWindow
+    QLineEdit, QPushButton, QMainWindow, QTableWidget
 
 from PyQt6.QtGui import QAction
 
@@ -21,6 +21,13 @@ class MainWindow(QMainWindow):
         about_action = QAction("About", self)
         help_menu_item.addAction(about_action)
         about_action.setMenuRole(QAction.MenuRole.NoRole)
+
+        self.table = QTableWidget()
+        self.table.setColumnCount(4)
+        self.table.setHorizontalHeaderLabels(("ID","Name", "Course", "Mobile"))
+        self.setCentralWidget(self.table)
+    def load_data(self):
+        self.table
 
 app = QApplication(sys.argv)
 age_calculator = MainWindow()
