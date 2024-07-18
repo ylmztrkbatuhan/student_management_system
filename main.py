47f79186615bcf13c0b8120158574fac4454252e
oboy21 <oboy11b@gmail.com>
2024-07-18 16:04:17 +0300

    inserting new records

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ class MainWindow(QMainWindow):
 
 
         add_student_action = QAction("Add Student")
+        add_student_action.triggered.connect(self.insert)
         file_menu_item.addAction(add_student_action)
 
         about_action = QAction("About", self)
@@ -37,7 +38,52 @@ class MainWindow(QMainWindow):
             self.table.insertRow(row_number)
             for column_number, data in enumerate(row_data):
                 self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
+        connection.close()
 
+
+    def insert(self):
+        dialog = InsertDialog()
+        dialog.exec()
+
+
+class InsertDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Insert Student Data")
+        self.setFixedWidth(300)
+        self.setFixedHeight(300)
+
+
+        layout = QVBoxLayout()
+
+        # ADD student name widget
+        self.student_name = QLineEdit()
+        self.student_name.setPlaceholderText("Name")
+        layout.addWidget(self.student_name)
+        # Add combo box courses
+        self.course_name = QComboBox()
+        courses = ["Biology","Math", "Astronomy", "Physics"]
+        self.course_name.addItems(courses)
+        layout.addWidget(self.course_name)
+
+        # Add mobile widget
+        self.mobile = QLineEdit()
+        self.mobile.setPlaceholderText("Name")
+        layout.addWidget(self.mobile)
+        # Add a submit button
+        button = QPushButton("Register")
+        button.clicked.connect(self.add_student)
+
+        self.setLayout(layout)
+    def add_student(self):
+        name = self.student_name.text()
+        course = self.course_name.itemText(self.course_name.currentIndex())
+        mobile = self.mobile.text()
+        connection = sqlite3.connect("database.db")
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO students (name, course, mobile)  VALUES (?, ?, ?)", (name, course, mobile))
+        connection.commit()
+        cursor.close()
         connection.close()
 
 app = QApplication(sys.argv)
